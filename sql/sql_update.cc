c284c93f31ab409fdbd466a0b449f2783987d4cd
Dave Gosselin <dave.gosselin@mariadb.com>
2025-01-08 15:18:03 -0500

    MDEV-35510 ASAN build crashes during bootstrap
    Decouple a DML result's cleanup from the destructor semantics, allowing
    cleanup to occur without simultaneously deallocating memory.
    
    The MYSQL_DML_DONE function would access the select_result_interceptor
    implementation (in either the cases of UPDATE or DELETE) after that
    interceptor instance's destructor had been called.  Most of the time
    this works because the instance was allocated on the statement memroot
    which continues to live on and no other allocations are made at this point.
    The interceptor instance's destructor would be called during either
    Sql_cmd_update::execute_inner or Sql_cmd_delete::execute_inner by an
    explicit call to delete.  Since the result instance lives on the
    memroot associated with the current command, we can avoid calling the
    destructor via delete and instead invoke a cleanup method, deferring
    the memory deallocation to free_root when the command's processing has
    completed.

--- sql/sql_update.cc
+++ sql/sql_update.cc
@@ -2300,6 +2300,12 @@ int multi_update::prepare2(JOIN *join)
 
 multi_update::~multi_update()
 {
+  cleanup();
+}
+
+
+void multi_update::cleanup()
+{
   TABLE_LIST *table;
   for (table= update_tables ; table; table= table->next_local)
   {
@@ -2319,10 +2325,12 @@ multi_update::~multi_update()
       }
     }
   }
-  if (copy_field)
-    delete [] copy_field;
+  if (copy_field) {
+    delete[] copy_field;
+    copy_field= nullptr;
+  }
   thd->count_cuted_fields= CHECK_FIELD_IGNORE;		// Restore this setting
-  DBUG_ASSERT(trans_safe || !updated || 
+  DBUG_ASSERT(trans_safe || !updated ||
               thd->transaction->all.modified_non_trans_table);
 }
 
@@ -3176,7 +3184,7 @@ bool Sql_cmd_update::execute_inner(THD *thd)
   if (result)
   {
     res= false;
-    delete result;
+    result->cleanup();
   }
 
   status_var_add(thd->status_var.rows_sent, thd->get_sent_row_count());
